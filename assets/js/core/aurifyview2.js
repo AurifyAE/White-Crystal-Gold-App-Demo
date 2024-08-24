bbc7bec65f1ab3b34766999cb1752389f7fa2649
Mohammed-Sabith-E <sabithchungam10@gmail.com>
2024-08-24 20:31:51 +0530

    first commit

---
+++ assets/js/core/aurifyview2.js
@@ -0,0 +1,420 @@
+// import { readSpreadValues } from '../core/spotrateDB.js';
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { app } from '../../../config/db.js';
+
+const script = document.createElement('script');
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.2.0/socket.io.js';
+document.head.appendChild(script);
+
+const socket = io('https://capital-server-9ebj.onrender.com/', {
+    query: { secret: 'aurify@123' }, // Pass secret key as query parameter
+});
+
+const firestore = getFirestore(app)
+
+socket.on("connect", () => {
+    console.log("Connected to WebSocket server");
+    requestMarketData(["GOLD", "SILVER"]);
+});
+
+// Request market data based on symbols
+function requestMarketData(symbols) {
+    socket.emit("request-data", symbols);
+}
+
+setInterval(fetchData1, 500);
+
+setInterval(() => {
+    fetchData()
+}, 500)
+
+showTable();
+silverTable()
+
+let askSpread, bidSpread, silverBidSpread, silverAskSpread, goldBuy, goldAskingPrice, goldBiddingPrice,
+    goldSell, silverBuy, silverSell, silverValue, goldHigh, goldLow, silverLow, silverHigh, silverAskingPrice, silverBiddingPrice;
+
+let goldData = {}
+let silverData = {}
+
+async function fetchData() {
+    socket.on('market-data', (data) => {
+        // console.log('Received gold value:', data);
+
+        if (data && data.symbol) {
+            if (data.symbol === "Gold") {
+                goldData = data;
+                // updateGoldUI();
+            } else if (data.symbol === "Silver") {
+                silverData = data;
+            }
+        } else {
+            console.warn("Received malformed market data:", data);
+        }
+
+
+        const value = goldData.bid;
+        goldHigh = goldData.high;
+        goldLow = goldData.low;
+        goldBuy = (value + bidSpread).toFixed(2);
+        goldSell = (value + bidSpread + askSpread + parseFloat(0.5)).toFixed(2);
+
+        const value2 = silverData.bid;
+        silverHigh = silverData.high;
+        silverLow = silverData.low;
+        silverBuy = (value2 + silverBidSpread).toFixed(2);
+        silverSell = (value2 + silverBidSpread + silverAskSpread + parseFloat(0.5)).toFixed(2);
+
+
+
+    });
+
+    var goldBuyUSD = (goldBuy ? goldBuy : '2511.56' / 31.103).toFixed(4);
+    goldBiddingPrice = (goldBuyUSD * 3.674).toFixed(4);
+
+    var goldSellUSD = (goldSell ? goldSell : '2512.60' / 31.103).toFixed(4);
+    goldAskingPrice = (goldSellUSD * 3.674).toFixed(4);
+
+    var silverBuyUSD = (silverBuy ? silverBuy : '27.823' / 31.103).toFixed(4);
+    silverBiddingPrice = (silverBuyUSD * 3.674).toFixed(4);
+
+    var silverSellUSD = (silverSell ? silverSell : '31.844' / 31.103).toFixed(4);
+    silverAskingPrice = (silverSellUSD * 3.674).toFixed(4);
+}
+
+// Function to Fetch Gold API Data
+async function fetchData1() {
+    try {
+        var currentGoldBuy = goldBuy ? goldBuy : '2511.56';
+        var currentGoldSell = goldSell ? goldSell : '2512.60';
+        var currentSilverBuy = silverBuy ? silverBuy : '27.823';
+        var currentSilverSell = silverSell ? silverSell : '31.844';
+
+
+        function updatePrice() {
+            var newGoldBuy = goldBuy ? goldBuy : '2511.56';
+            var newGoldSell = goldSell ? goldSell : '2512.60';
+            var newSilverBuy = silverBuy ? silverBuy : '27.823';
+            var newSilverSell = silverSell ? silverSell : '31.844';
+
+            var element1 = document.getElementById("goldInputLow");
+            var element2 = document.getElementById("goldInputHigh");
+            var element3 = document.getElementById("silverInputLow");
+            var element4 = document.getElementById("silverInputHigh");
+
+            element1.innerHTML = newGoldBuy;
+            element2.innerHTML = newGoldSell;
+            element3.innerHTML = newSilverBuy;
+            element4.innerHTML = newSilverSell;
+
+            // Determine color for each element
+            var color1;
+            var fontColor1;
+            if (newGoldBuy > currentGoldBuy) {
+                color1 = "green";
+                fontColor1 = "white"
+            } else if (newGoldBuy < currentGoldBuy) {
+                color1 = "red";
+                fontColor1 = "white"
+            } else {
+                color1 = "white"; // Set to white if no change
+                fontColor1 = "black"
+            }
+
+            var color2;
+            var fontColor2;
+            if (newGoldSell > currentGoldSell) {
+                color2 = "green";
+                fontColor2 = "white"
+            } else if (newGoldSell < currentGoldSell) {
+                color2 = "red";
+                fontColor2 = "white"
+            } else {
+                color2 = "white"; // Set to white if no change
+                fontColor2 = "black"
+            }
+
+            var color3;
+            var fontColor3;
+            if (newSilverBuy > currentSilverBuy) {
+                color3 = "green";
+                fontColor3 = "white"
+            } else if (newSilverBuy < currentSilverBuy) {
+                color3 = "red";
+                fontColor3 = "white"
+            } else {
+                color3 = "white"; // Set to white if no change
+                fontColor3 = "black"
+            }
+
+            var color4;
+            var fontColor4;
+            if (newSilverSell > currentSilverSell) {
+                color4 = "green";
+                fontColor4 = "white"
+            } else if (newSilverSell < currentSilverSell) {
+                color4 = "red";
+                fontColor4 = "white"
+            } else {
+                color4 = "white"; // Set to white if no change
+                fontColor4 = "black"
+            }
+
+            element1.style.backgroundColor = color1;
+            element2.style.backgroundColor = color2;
+            element3.style.backgroundColor = color3;
+            element4.style.backgroundColor = color4;
+
+            element1.style.color = fontColor1;
+            element2.style.color = fontColor2;
+            element3.style.color = fontColor3;
+            element4.style.color = fontColor4;
+
+
+            currentGoldBuy = newGoldBuy;
+            currentGoldSell = newGoldSell;
+            currentSilverBuy = newSilverBuy;
+            currentSilverSell = newSilverSell;
+
+            setTimeout(updatePrice, 300);
+        }
+
+
+        updatePrice();
+
+        document.getElementById("lowLabelGold").innerHTML = goldLow ? goldLow : '2484.32';
+        document.getElementById("highLabelGold").innerHTML = goldHigh ? goldHigh : '2518.31';
+        document.getElementById("lowLabelSilver").innerHTML = silverLow ? silverLow : '28.937';
+        document.getElementById("highLabelSilver").innerHTML = silverHigh ? silverHigh : '29.879';
+
+        // var element;
+
+        // // LowLabelGold
+        // element = document.getElementById("lowLabelGold");
+        // element.style.backgroundColor = "red";
+
+        // // HighLabelGold
+        // element = document.getElementById("highLabelGold");
+        // element.style.backgroundColor = "green";
+
+        // // LowLabelSilver
+        // element = document.getElementById("lowLabelSilver");
+        // element.style.backgroundColor = "red";
+
+        // // HighLabelSilver
+        // element = document.getElementById("highLabelSilver");
+        // element.style.backgroundColor = "green";
+    } catch (error) {
+        console.error('Error fetching gold and silver values:', error);
+    }
+}
+
+
+async function readSpreadValues() {
+    try {
+        const uid = 'XF5cxSUq2wWa4jwVYgInRqZRcqx1';
+        if (!uid) {
+            console.error('User not authenticated');
+            throw new Error('User not authenticated');
+        }
+
+        const spreadCollection = collection(firestore, `users/${uid}/spread`);
+        const querySnapshot = await getDocs(spreadCollection);
+
+        const spreadDataArray = [];
+        querySnapshot.forEach((doc) => {
+            const spreadData = doc.data();
+            const spreadDocId = doc.id;
+            spreadDataArray.push({ id: spreadDocId, data: spreadData });
+        });
+
+        // console.log(spreadDataArray);
+
+        return spreadDataArray;
+    } catch (error) {
+        console.error('Error reading data from Firestore: ', error);
+        throw error;
+    }
+}
+
+async function displaySpreadValues() {
+    try {
+        const spreadDataArray = await readSpreadValues();
+
+        spreadDataArray.forEach((spreadData) => {
+            askSpread = spreadData.data.editedAskSpreadValue || 0;
+            bidSpread = spreadData.data.editedBidSpreadValue || 0;
+            silverAskSpread = spreadData.data.editedAskSilverSpreadValue || 0;
+            silverBidSpread = spreadData.data.editedBidSilverSpreadValue || 0;
+        });
+    } catch (error) {
+        console.error('Error reading spread values: ', error);
+        throw error;
+    }
+}
+
+displaySpreadValues();
+
+// Function to read data from the Firestore collection
+async function readData() {
+    // Get the UID of the authenticated user
+    const uid = 'XF5cxSUq2wWa4jwVYgInRqZRcqx1';
+
+    if (!uid) {
+        console.error('User not authenticated');
+        return Promise.reject('User not authenticated');
+    }
+
+    const querySnapshot = await getDocs(collection(firestore, `users/${uid}/commodities`));
+    const result = [];
+    querySnapshot.forEach((doc) => {
+        result.push({
+            id: doc.id,
+            data: doc.data()
+        });
+    });
+    return result;
+}
+
+
+async function showTable() {
+    try {
+        const tableData = await readData();
+        const tableBody = document.getElementById("tableBodyTV");
+
+        // Loop through the tableData
+        for (const data of tableData) {
+            // Assign values from data to variables
+            const metalInput = data.data.metal;
+            const purityInput = data.data.purity;
+            const unitInput = data.data.unit;
+            const weightInput = data.data.weight;
+            const sellAEDInput = data.data.sellAED;
+            const buyAEDInput = data.data.buyAED;
+            const sellPremiumInputAED = data.data.sellPremiumAED;
+            const buyPremiumInputAED = data.data.buyPremiumAED;
+
+            let metal;
+            let purity;
+            if (weightInput === "KG") {
+                metal = "GOLD";
+                purity = purityInput;
+            } else if (weightInput === "TTB") {
+                metal = "GOLD";
+                purity = "TEN TOLA";
+            } else if (weightInput === "GM") {
+                metal = "GOLD";
+                purity = purityInput;
+            } else {
+                metal = metalInput;
+                purity = purityInput;
+            }
+
+            // Create a new table row for data
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
+                  <td style="text-align: right;">${metal}</td>
+                  <td style="text-align: left; font-size:12px; font-weight: 600;">${purity}</td>
+                  <td>${unitInput} ${weightInput}</td>
+                  <td id="sellAED">0</td>
+              `;
+            // Append the new row to the table body
+            tableBody.appendChild(newRow);
+
+            // Create an empty row (if not the last row)
+            if (data !== tableData[tableData.length - 1]) {
+                tableBody.insertAdjacentHTML(
+                    "beforeend",
+                    '<tr><td colspan="5" style="height: 4px;"></td></tr>'
+                );
+            }
+
+            displaySpreadValues();
+
+            setInterval(async () => {
+                let weight = weightInput;
+                let unitMultiplier = 1;
+
+                // Adjust unit multiplier based on the selected unit
+                if (weight === "GM") {
+                    unitMultiplier = 1;
+                } else if (weight === "KG") {
+                    unitMultiplier = 1000;
+                } else if (weight === "TTB") {
+                    unitMultiplier = 116.64;
+                } else if (weight === "TOLA") {
+                    unitMultiplier = 11.664;
+                } else if (weight === "OZ") {
+                    unitMultiplier = 31.1034768;
+                }
+
+                let sellPremium = sellPremiumInputAED || 0;
+                let buyPremium = buyPremiumInputAED || 0;
+                let askSpreadValue = askSpread || 0;
+                let bidSpreadValue = bidSpread || 0;
+
+                if (weight === "GM") {
+                    // Update the sellAED and buyAED values for the current
+                    newRow.querySelector("#sellAED").innerText = parseFloat(
+                        (
+                            goldAskingPrice *
+                            unitInput *
+                            unitMultiplier *
+                            (purityInput / Math.pow(10, purityInput.length)) +
+                            parseFloat(sellPremium)
+                        ).toFixed(2)
+                    );
+                    newRow.querySelector("#buyAED").innerText = (
+                        goldBiddingPrice *
+                        unitInput *
+                        unitMultiplier *
+                        (purityInput / Math.pow(10, purityInput.length)) +
+                        parseFloat(buyPremium)
+                    ).toFixed(2);
+                } else {
+                    // Update the sellAED and buyAED values for the current row
+                    const sellAEDValue = parseFloat(
+                        (
+                            goldAskingPrice *
+                            unitInput *
+                            unitMultiplier *
+                            (purityInput / Math.pow(10, purityInput.length)) +
+                            parseFloat(sellPremium)
+                        ).toFixed(4)
+                    );
+                    const buyAEDValue = parseInt(
+                        goldBiddingPrice *
+                        unitInput *
+                        unitMultiplier *
+                        (purityInput / Math.pow(10, purityInput.length)) +
+                        parseFloat(buyPremium)
+                    ).toFixed(0);
+
+                    newRow.querySelector("#sellAED").innerText =
+                        parseInt(sellAEDValue).toFixed(0); // Round to remove decimals
+                    newRow.querySelector("#buyAED").innerText =
+                        parseInt(buyAEDValue).toFixed(0); // Round to remove decimals
+                }
+            }, 500);
+        }
+    } catch (error) {
+        console.error("Error reading data:", error);
+    }
+}
+
+function silverTable() {
+    setInterval(() => {
+
+        // Silver 1GM Table Value
+        // const buyValue = parseFloat(
+        //     (parseFloat(silverBiddingPrice) + parseFloat(silverBidSpread) || 0) * 1000
+        // ).toFixed(0);
+        const sellValue = parseFloat(
+            (parseFloat(silverAskingPrice) + 0.05 + parseFloat(silverAskSpread) || 0) * 1000
+        ).toFixed(0);
+
+        // document.getElementById("silverBidTd").textContent = buyValue ? buyValue : 0;
+        document.getElementById("silverAskTd").textContent = sellValue ? sellValue : 0;
+        //console.log(parseFloat(silver));
+    }, 500);
+}
---
+++ assets/js/core/aurifyview3.js
@@ -0,0 +1,279 @@
+import { getFirestore, deleteDoc, getDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+import { app } from '../../../config/db.js';
+
+const script = document.createElement("script");
+script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.2.0/socket.io.js";
+document.head.appendChild(script);
+
+const socket = io("https://capital-server-9ebj.onrender.com", {
+    query: { secret: "aurify@123" }, // Pass secret key as query parameter
+});
+
+const firestore = getFirestore(app);
+
+socket.on("connect", () => {
+    console.log("Connected to WebSocket server");
+    requestMarketData(["GOLD", "SILVER"]);
+});
+
+// Request market data based on symbols
+function requestMarketData(symbols) {
+    socket.emit("request-data", symbols);
+}
+
+setInterval(() => {
+    fetchData();
+    document.getElementById('goldRateValue').textContent = '$' + goldValue.toFixed(2);
+}, 500);
+
+let goldValue, silverValue, alertValue, currentGoldValue, alert;
+
+let goldData = {};
+
+
+// Function to Fetch Gold API Data
+async function fetchData() {
+    try {
+        socket.on("market-data", (data) => {
+            if (data && data.symbol) {
+                if (data.symbol === "Gold") {
+                    goldData = data;
+                    // updateGoldUI();
+                } else if (data.symbol === "Silver") {
+                    silverData = data;
+                }
+            } else {
+                console.warn("Received malformed market data:", data);
+            }
+
+            const value = goldData.bid;
+            goldValue = value;
+            currentGoldValue = value;
+            playAlert(value)
+        });
+
+    } catch (error) {
+        console.error('Error fetching gold and silver values:', error);
+    }
+    alertInitialValue()
+}
+
+// Show Alert values from Firebase
+readData()
+    .then((result) => {
+        // console.log('Document data:', result.data.alertValue);
+        document.getElementById('displayValue').innerHTML = result.data.alertValue;
+        setAlertValue(result.data.alertValue);
+    })
+    .catch((error) => {
+        console.error(error);
+        document.getElementById('displayValue').style.display = 'none';
+        document.getElementById('alertDeleteBtn').style.display = 'none';
+        document.getElementById('display').style.display = 'none';
+    });
+
+
+
+// Event Listener for Delete Alert Btn
+document.getElementById('alertDeleteBtn').addEventListener('click', async () => {
+    console.log('delete btn');
+    try {
+        const uid = 'Tm9O3jd5jHMX4YGkIecSZcDMG5m1';
+
+        if (!uid) {
+            console.error('User not authenticated');
+            throw new Error('User not authenticated');
+        }
+
+        await deleteDoc(doc(firestore, `users/${uid}/alert/alertValue`));
+        document.getElementById('displayValue').style.display = 'none';
+        document.getElementById('alertDeleteBtn').style.display = 'none';
+        document.getElementById('display').style.display = 'none';
+
+        console.log('Document deleted successfully');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+});
+
+
+
+// Function to set Alert initial Value
+function alertInitialValue() {
+    var currentValue = $("#slider").roundSlider("option", "value");
+    if (currentValue === 50) {
+        // console.log(currentValue);
+        document.getElementById('value').innerHTML = goldValue.toFixed(0);
+    }
+}
+
+// Function to show Alert
+function rateAlert() {
+    var radius = 120; // Default radius
+
+    // Check screen width and adjust radius accordingly
+    if ($(window).width() >= 600) {
+        radius = 200; // Adjusted radius for smaller devices
+    }
+
+    if ($(window).width() >= 900) {
+        radius = 300; // Adjusted radius for smaller devices
+    }
+
+    if ($(window).width() >= 1024) {
+        radius = 350; // Adjusted radius for smaller devices
+    }
+
+    // Initialize the round slider on the element
+    $("#slider").roundSlider({
+        radius: radius,
+        circleShape: "half-top",
+        sliderType: "mid-range",
+        showTooltip: false,
+        value: 50,
+        min: 0,
+        max: 100,
+        lineCap: "round",
+        handleSize: 25
+    });
+
+
+    // Set up a callback function for the value change event
+    $("#slider").on("drag", function (event) {
+        // Get the current value
+        var currentValue = $("#slider").roundSlider("option", "value");
+
+        if (currentValue <= 50) {
+            alertValue = Math.round(goldValue - 50 + currentValue);
+            document.getElementById('value').innerHTML = alertValue;
+        } else {
+            alertValue = Math.round(goldValue + currentValue - 50);
+            document.getElementById('value').innerHTML = alertValue;
+        }
+        console.log("Current Value:", currentValue);
+    });
+}
+
+rateAlert()
+
+// Function to read data from the Firestore collection
+async function readData() {
+    // Get the UID of the authenticated user
+    const uid = 'Tm9O3jd5jHMX4YGkIecSZcDMG5m1';
+
+    if (!uid) {
+        console.error('User not authenticated');
+        return Promise.reject('User not authenticated');
+    }
+
+    const docRef = doc(firestore, `users/${uid}/alert/alertValue`);
+
+    try {
+        const docSnapshot = await getDoc(docRef);
+
+        if (docSnapshot.exists()) {
+            // Document exists, retrieve its data
+            const result = {
+                id: docSnapshot.id,
+                data: docSnapshot.data()
+            };
+            return result;
+        } else {
+            console.error('Document does not exist');
+            return Promise.reject('Document does not exist');
+        }
+    } catch (error) {
+        console.error('Error getting document:', error);
+        return Promise.reject('Error getting document');
+    }
+}
+
+// Function to save data to the Firestore collection
+async function saveData(data) {
+    // Get the UID of the authenticated user
+    const uid = 'Tm9O3jd5jHMX4YGkIecSZcDMG5m1';
+
+    if (!uid) {
+        console.error('User not authenticated');
+        return Promise.reject('User not authenticated');
+    }
+
+    const docRef = doc(firestore, `users/${uid}/alert/alertValue`);
+
+    await setDoc(docRef, data);
+
+    console.log('Document written');
+}
+
+//////////////////
+// Event Listener to Edit Alert Value
+document.getElementById('value').addEventListener('input', () => {
+    // Update alertValue with the edited content
+    alertValue = document.getElementById('value').textContent;
+    console.log(alertValue);
+});
+
+// Optionally, you can add a click event to enable content editing on click
+document.getElementById('value').addEventListener('click', () => {
+    // Set contentEditable to true when the element is clicked
+    document.getElementById('value').contentEditable = true;
+});
+//////////////////
+
+////////////////////
+// Event Listener for Set Alert Button Click
+document.getElementById('alertBtn').addEventListener('click', () => {
+    let value = parseFloat(alertValue);
+    let value2 = parseFloat(goldValue);
+
+
+    const value3 = value.toFixed(0);
+    const value4 = value2.toFixed(0);
+
+    console.log(alertValue);
+    saveData({
+        alertValue: alertValue
+    })
+
+    let alert;
+    readData()
+        .then((result) => {
+            // console.log('Document data:', result);
+            document.getElementById('displayValue').innerHTML = result.data.alertValue;
+            setAlertValue(result.data.alertValue);
+            document.getElementById('displayValue').style.display = 'inline-block';
+            document.getElementById('alertDeleteBtn').style.display = 'inline-block';
+            document.getElementById('display').style.display = 'inline-block';
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+    if (alert === value4) {
+        document.getElementById('xyz').play();
+        // alert("Thank you!");
+    }
+});
+
+// Function to Set Alert Value Globaly
+function setAlertValue(value) {
+    alert = value;
+}
+
+// Function to Play Alert
+function playAlert() {
+    let value = parseFloat(goldValue);
+
+    const value2 = value.toFixed(0);
+    const value3 = alert.toFixed(0);
+    console.log(value2, value3);
+
+    if (value3 === value2) {
+        // alert("Thank you!");
+        document.getElementById('xyz').play();
+    }
+}
+
+playAlert()
+
